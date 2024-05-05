2c056b2f7edafb1026f86fe55684c5cb2f893bc4
sarankirthic <spskartist@gmail.com>
2024-05-06 00:21:41 +0530

    complete revamp

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-var canvas=document.getElementById("canvas");
-var ctx=canvas.getContext("2d");
-canvas.height=window.innerHeight;
-canvas.width=window.innerWidth;
+// Get the current page URL
+const currentPage = window.location.pathname;
+const pageNames = {
+	'/': 'home',
+	'/about/about.html': 'about',
+};
 
-var j="私は誰ですか？ああ、それは素晴らしいパズルです。1!2@3#4$5%6^7&8*9(0)ﾊﾐﾋｰｳｼﾅﾓﾆｻﾜﾂｵﾘｱﾎﾃﾏｹﾒｴｶｷﾑﾕﾗｾﾈｽﾀﾇﾍ".split("");
+// Function to generate breadcrumbs
+function generateCoffee() {
+	const coffeeContainer = document.querySelector('.coffeespill');
+	const pathSegments = currentPage.split('/').filter(
+			segment => segment !== '');  // Split the URL and remove empty segments
 
-function draw(){
-	ctx.fillStyle="rgba(0, 0,0,0.05)";
-	ctx.fillRect(0,0,canvas.width,canvas.height);
-	ctx.fillStyle="#0F0";
-	ctx.font=font_size+"px arial";
-	for(var a=0;a<drops.length;a++){
-		var b=j[Math.floor(Math.random()*j.length)];
-		ctx.fillText(b,a*font_size,drops[a]*font_size);
-		drops[a]*font_size>canvas.height&&Math.random()>.975&&(drops[a]=0);
-		drops[a]++;
-	}
+	let coffeeStain = '<a href="/">home</a>';
+	
+
+	coffeeContainer.innerHTML = coffeeStain;
 }
 
-for(var font_size=10,columns=canvas.width/font_size,drops=[],x=0;x<columns;x++){
-	drops[x]=1;
-}
-setInterval(draw,50);
+// Call the function to generate coffee
+generateCoffee();
