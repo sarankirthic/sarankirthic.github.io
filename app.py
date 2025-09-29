98e2ce7d4925706d885975bd9bd96493fe997219
saran <spskartist@gmail.com>
2025-09-30 00:19:00 +0530

    commented part of code to prevent errors while deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mail import Mail, Message
-from config import Config
+# from config import Config
 
 app = Flask(__name__)
 
@@ -25,29 +25,29 @@ def about():
     return render_template('about.html', title='About')
 
 
-mail = Mail(app)
-
-@app.route('/contact')
-def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-
-        msg = Message(subject="New Contact Form Message",
-                      sender=Config.MAIL_USERNAME,
-                      recipients=["sarankirthic@gmail.com", "spskartist@gmail.com"])
-        msg.body = f"From: {name} <{email}>\n\n{message}"
-
-        try:
-            mail.send(msg)
-            flash('Email send Successfully', 'success')
-        except Exception as e:
-            flash(f'Failed to send Email: {str(e)}', 'danger')
-
-        return redirect(url_for('contact'))
-
-    return render_template('contact.html', title='Contact')
+# mail = Mail(app)
+#
+# @app.route('/contact')
+# def contact():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         message = request.form['message']
+#
+#         msg = Message(subject="New Contact Form Message",
+#                       sender=Config.MAIL_USERNAME,
+#                       recipients=["sarankirthic@gmail.com", "spskartist@gmail.com"])
+#         msg.body = f"From: {name} <{email}>\n\n{message}"
+#
+#         try:
+#             mail.send(msg)
+#             flash('Email send Successfully', 'success')
+#         except Exception as e:
+#             flash(f'Failed to send Email: {str(e)}', 'danger')
+#
+#         return redirect(url_for('contact'))
+#
+#     return render_template('contact.html', title='Contact')
 
 
 @app.route('/secret')
